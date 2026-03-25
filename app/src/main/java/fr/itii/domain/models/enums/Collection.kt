843c74bcb099ece7581f7482eec739ed6458ca41
Enzo Dubois <enzodubois2064@gmail.com>
2026-03-25 19:20:34 +0100

    moi

--- app/src/main/java/fr/itii/domain/models/enums/Collection.kt
+++ app/src/main/java/fr/itii/domain/models/enums/Collection.kt
@@ -3,6 +3,5 @@ package fr.itii.domain.models.enums
 enum class Collection(val nameTable: String) {
     USER("users"),
     EVENT("events"),
-    CITY("cities"),
-    REGISTORY("registories")
+    CITY("cities")
 }
